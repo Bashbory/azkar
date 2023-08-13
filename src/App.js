af931951e21a7aa438209f2a2e2b8a048d96e263
Bashbory <eightmick912@gmail.com>
2023-08-13 21:12:58 +0300

    hi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Azkar from "./Azkar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App grid place-items-center  h-screen bg-gradient-to-r from-[#024c46] to-[#206d4d] azkar
+    bg-cover bg-center "
+    >
+      <Azkar />
     </div>
   );
 }
